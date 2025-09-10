0582d2d783d36f75ef641e7b5c3028c35f595a65
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-09-10 14:29:49 +1200

    did level design + debuged explosion hitbox

--- laserbeam.gd
+++ laserbeam.gd
@@ -6,13 +6,16 @@ extends Area2D
 func _process(_delta):
 	position.y += speed
 	if $RayCast2D.is_colliding():
+		var collider = $RayCast2D.get_collider()
+		if collider != null and collider.name == "player":
+			var spawn_point = get_node("/root/game/SpawnPoint")
+			collider.global_position = spawn_point.global_position
 		speed = 0
 		var BOOM = BOOM_prefab.instantiate()
 		BOOM.position = position
 		get_parent().add_child(BOOM)
 		queue_free()
 		
-
 func _on_body_entered(body):
 	if body.name == "player":
 		body.respawn()
